9be17b4a1bf94a142880711fe784e3c5abeebf94
ManojSelenium <manojqa5006@gmail.com>
2017-11-01 20:29:50 +0530

    hi this is murali

---
+++ MavenFouthProject/src/main/java/com/utility/SetUp.java
@@ -0,0 +1,5 @@
+package com.utility;
+
+public interface SetUp {
+String AUTOMATIONURL="http://automationpractice.com/index.php";
+}
---
+++ MavenFouthProject/src/test/java/com/automationpractcs/SecondTest.java
@@ -0,0 +1,13 @@
+package com.automationpractcs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SecondTest {
+	public void At_2(){
+		  System.setProperty("webdriver.gecko.driver","D:\\JARS\\geckodriver.exe");
+		  WebDriver driver=new FirefoxDriver();
+		  driver.get("http://www.google.co.in");
+	}
+
+}
