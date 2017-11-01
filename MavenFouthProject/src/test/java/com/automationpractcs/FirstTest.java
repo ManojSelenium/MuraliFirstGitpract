b5487e29cb8627786aac7272ebd7e7c0d956a661
ManojSelenium <manojqa5006@gmail.com>
2017-11-01 20:25:30 +0530

    hi m

---
+++ MavenFouthProject/src/test/java/com/automationpractcs/FirstTest.java
@@ -0,0 +1,16 @@
+package com.automationpractcs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import com.utility.SetUp;
+
+public class FirstTest implements SetUp{
+  @Test
+  public void AT_1() {
+	  System.setProperty("webdriver.gecko.driver","D:\\JARS\\geckodriver.exe");
+	  WebDriver driver=new FirefoxDriver();
+	  driver.get(AUTOMATIONURL);
+  }
+}
